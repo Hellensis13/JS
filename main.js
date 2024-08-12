a0440767c1a6d330ed01bca8c9662121e0ff9a8a
Hellensis13 <el.tovkach@gmail.com>
2024-08-12 11:58:48 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+// Varianta 1
+// ex. 1
+
+let vadimWeight1 = 78;
+const vadimHeight1 = 1.69;
+let alexWeight1 = 92;
+const alexHeight1 = 1.95;
+
+// ex. 2
+let vadimBmi1 = vadimWeight1 / vadimHeight1 ** 2;
+let alexBmi1 = alexWeight1 / alexHeight1 **2;
+console.log("Vadim BMI 1 is " + vadimBmi1);
+console.log("Alex BMI 1 is " + alexBmi1);
+
+
+// ex. 3
+let markHigherBMI1 = vadimBmi1 > alexBmi1;
+
+
+// Varianta 2 
+// ex. 1
+
+let vadimWeight2 = 95;
+const vadimHeight2 = 1.88;
+let alexWeight2 = 85;
+const alexHeight2 = 1.76;
+
+// ex. 2
+let vadimBmi2 = vadimWeight2 / vadimHeight2 ** 2;
+let alexBmi2 = alexWeight2 / alexHeight2 **2;
+console.log("Vadim BMI 2 is " + vadimBmi2);
+console.log("Alex BMI 2 is " + alexBmi2);
+
+
+// ex. 3
+let markHigherBMI2 = vadimBmi2 > alexBmi2;
